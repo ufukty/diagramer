6fbe7548d222bce85b384df504f77b46113f61ba
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-07-14 10:24:04 +0300

    pkg/sequence/doc: inits

---
+++ pkg/sequence/doc/doc.go
@@ -0,0 +1,28 @@
+// Package doc provides the complete AST-like structure for the
+// document which is ready to lay out. It makes every detail
+// implicitly mentioned by the user explicit.
+package doc
+
+import (
+	"github.com/ufukty/diagramer/pkg/sequence/ast"
+)
+
+type Lifeline struct {
+	*ast.Lifeline // if available
+	Track         int
+	StepCreate    int
+	StepDestroy   int // the steps the lifeline gets created and destroyed
+}
+
+type Message struct {
+	Ast       *ast.Message
+	TrackFrom int
+	TrackTo   int
+	Step      int
+}
+
+type Diagram struct {
+	Ast       *ast.Diagram
+	Lifelines []*Lifeline
+	Messages  []*Message
+}
