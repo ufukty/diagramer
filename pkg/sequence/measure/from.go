a8836ab5a6981d8b659a8644daa8d2a4931fe0d9
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-07-15 21:06:25 +0300

    pkg/seq/measure: changes `FromDoc` to accept its `Font` dependency needed for measuring content width

--- pkg/sequence/measure/from.go
+++ pkg/sequence/measure/from.go
@@ -1,7 +1,10 @@
 package measure
 
-import "github.com/ufukty/diagramer/pkg/sequence/doc"
+import (
+	"github.com/ufukty/diagramer/pkg/sequence/doc"
+	"github.com/ufukty/diagramer/pkg/sequence/internal/text"
+)
 
-func FromDoc(d *doc.Diagram) *Diagram {
+func FromDoc(d *doc.Diagram, font *text.Font) *Diagram {
 	return nil
 }
