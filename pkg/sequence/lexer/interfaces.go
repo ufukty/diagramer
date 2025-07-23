40508df4776235dfb6f0d9efa61dad05d1854281
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-07-23 11:50:03 +0300

    pkg/seq/lexer: adds missing function bodies on Line interface conformance methods

--- pkg/sequence/lexer/interfaces.go
+++ pkg/sequence/lexer/interfaces.go
@@ -4,21 +4,21 @@ type Line interface {
 	_line()
 }
 
-func (*Activate) _line()
-func (*Alt) _line()
-func (*And) _line()
-func (*Box) _line()
-func (*Break) _line()
-func (*Create) _line()
-func (*Critical) _line()
-func (*Deactivate) _line()
-func (*Destroy) _line()
-func (*Else) _line()
-func (*End) _line()
-func (*LifelineDecl) _line()
-func (*Loop) _line()
-func (*Message) _line()
-func (*Note) _line()
-func (*Option) _line()
-func (*Parallel) _line()
-func (*WideNote) _line()
+func (*Activate) _line()     {}
+func (*Alt) _line()          {}
+func (*And) _line()          {}
+func (*Box) _line()          {}
+func (*Break) _line()        {}
+func (*Create) _line()       {}
+func (*Critical) _line()     {}
+func (*Deactivate) _line()   {}
+func (*Destroy) _line()      {}
+func (*Else) _line()         {}
+func (*End) _line()          {}
+func (*LifelineDecl) _line() {}
+func (*Loop) _line()         {}
+func (*Message) _line()      {}
+func (*Note) _line()         {}
+func (*Option) _line()       {}
+func (*Parallel) _line()     {}
+func (*WideNote) _line()     {}
