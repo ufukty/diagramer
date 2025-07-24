ca2dfa431d965c0be9588c40ba86c6252efa59d8
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-07-24 06:01:51 +0300

    pkg/seq/lexer: implements check methods for rest of the node types

--- pkg/sequence/lexer/checkers.go
+++ pkg/sequence/lexer/checkers.go
@@ -2,56 +2,60 @@ package lexer
 
 import "strings"
 
+func startsWith(line, word string) bool {
+	return strings.Split(line, " ")[0] == word
+}
+
 func (Activate) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "activate")
 }
 
 func (Alt) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "alt")
 }
 
 func (And) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "and")
 }
 
 func (Box) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "box")
 }
 
 func (Break) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "break")
 }
 
 func (Create) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "create")
 }
 
 func (Critical) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "critical")
 }
 
 func (Deactivate) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "deactivate")
 }
 
 func (Destroy) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "destroy")
 }
 
 func (Else) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "else")
 }
 
 func (End) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "end")
 }
 
 func (LifelineDecl) check(line string) bool {
-	return strings.HasPrefix(line, "participant") || strings.HasPrefix(line, "actor")
+	return startsWith(strings.TrimSpace(line), "participant") || startsWith(strings.TrimSpace(line), "actor")
 }
 
 func (Loop) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "loop")
 }
 
 func (Message) check(line string) bool {
@@ -59,17 +63,17 @@ func (Message) check(line string) bool {
 }
 
 func (Note) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "note") && !strings.Contains(strings.Split(line, ":")[0], ",")
 }
 
 func (Option) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "option")
 }
 
 func (Parallel) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "parallel")
 }
 
 func (WideNote) check(line string) bool {
-	panic("not implemented")
+	return startsWith(strings.TrimSpace(line), "note") && strings.Contains(strings.Split(line, ":")[0], ",")
 }
